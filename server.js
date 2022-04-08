8ac696d1ac7267a46082fd7cec16e53b86dc35ad
Mit Shah <mitshah2406@gmail.com>
2022-04-08 16:50:55 +0530

    Initial

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
+
+
+//creating app
+const app = express()
+
+
+//paths
+const views = path.join(__dirname, '/resources/views')
+// const layouts = path.join(__dirname, '/resources/layouts')
+
+//set template engine
+// app.use(expressLayout)
+app.set('view engine','ejs')
+app.set('views',views)
+
+
+//sample route
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+
+//defining port
+const PORT = process.env.PORT || 4000;
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Connected to ${PORT}`);
+})
